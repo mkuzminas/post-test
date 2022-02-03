02a959bdd51d793c2b9c5eac99bb2d9d7f62f884
mkuzminas <marius.kuzminas@gmail.com>
2022-02-03 20:03:55 +0200

    Submit data added

---
+++ script.js
@@ -0,0 +1,25 @@
+const url = "https://jsonplaceholder.typicode.com/posts"
+fetch(url)
+    .then((response) => response.json())
+    .then((posts) => {
+        console.log(posts);
+        renderPosts(posts);
+    });
+
+function renderPosts(posts) {
+    posts.forEach(post => {
+        const divNode = document.createElement('div');
+        const anchorNode = document.createElement('a');
+        anchorNode.href = "#";
+        anchorNode.innerText = post.id;
+
+        const spanNode = document.createElement('span');
+        spanNode.innerText = post.title;
+
+        divNode.append(anchorNode);
+        divNode.append(spanNode);
+        divNode.setAttribute("class", "post");
+
+        document.body.append(divNode)
+    })
+}
